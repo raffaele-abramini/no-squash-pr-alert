2085d51a7da01dddac1cf7f2fca756608e5fd737
rabramini <rabramini@twilio.com>
2019-06-19 13:42:53 +0100

    Continue script

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,8 @@
-alert("yeh");
+// const mergeWithoutSquashButton = document.querySelector('.btn-group-merge .js-details-target');
+const mergeWithoutSquashButton = document.querySelector('.btn-group-merge .js-details-target');
+
+mergeWithoutSquashButton.addEventListener("click", (e) => {
+  if (!window.confirm("Are you sure you want to merge WITHOUT squashing your commits?")) {
+    e.stopImmediatePropagation();
+  }
+});
