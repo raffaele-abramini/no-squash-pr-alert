ba93450344201d071b4d4e590400ad3c35ac1259
oldirony <raffaele.abramini@gmail.com>
2019-06-23 11:43:07 +0100

    Quick contentScript refactor

--- contentScript.js
+++ contentScript.js
@@ -5,16 +5,16 @@
   const rebaseButton = document.querySelector('.btn-group-rebase button');
   const message = 'Are you sure you want to merge this PR WITHOUT squashing your commits?';
 
-  if (!mergeButton) {
-    return;
-  }
-
   const mergeAlertCb = e => {
     if (!window.confirm(message)) {
       e.stopImmediatePropagation();
     }
   };
 
-  mergeButton.addEventListener('click', mergeAlertCb, false);
-  rebaseButton.addEventListener('click', mergeAlertCb, false);
+  if (!mergeButton) {
+    mergeButton.addEventListener('click', mergeAlertCb, false);
+  }
+  if (!mergeButton) {
+    rebaseButton.addEventListener('click', mergeAlertCb, false);
+  }
 })();
